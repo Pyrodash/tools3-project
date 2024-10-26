91e48ae0758465bba2b764f7e9c680d851ab4c5c
Pyrodash <18378976+Pyrodash@users.noreply.github.com>
2024-10-26 16:52:26 +0300

    Added linter

---
+++ backend/eslint.config.js
@@ -0,0 +1,31 @@
+import globals from 'globals'
+import pluginJs from '@eslint/js'
+import pluginPrettier from 'eslint-plugin-prettier'
+import configPrettier from 'eslint-config-prettier'
+
+export default [
+    {
+        languageOptions: {
+            globals: globals.node,
+        },
+    },
+    pluginJs.configs.recommended,
+    {
+        plugins: {
+            prettier: pluginPrettier,
+        },
+        rules: {
+            // Enables Prettier rules as ESLint rules
+            'prettier/prettier': 'error',
+            'no-unused-vars': [
+                'warn',
+                {
+                    argsIgnorePattern: '^_',
+                    varsIgnorePattern: '^_',
+                    caughtErrorsIgnorePattern: '^_',
+                },
+            ],
+        },
+    },
+    configPrettier, // Disables rules that conflict with Prettier
+]
